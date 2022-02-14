cf12aa0f135b1ffec00892e446e396736d3ff2f1
MarcusVMendes <marcus.mendes89@gmail.com>
2022-02-14 15:10:51 -0300

    Initial frontend structure

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => res.send({ message: "Hello World"}))
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
